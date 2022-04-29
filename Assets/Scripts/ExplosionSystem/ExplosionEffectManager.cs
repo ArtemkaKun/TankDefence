c432d430e90ba216e17ca9fa6735ae3679222917
Artem Yurchenko <zumikq78@gmail.com>
2022-04-29 19:59:50 +0200

    ExplosionEffectManager class was implemented

---
+++ Assets/Scripts/ExplosionSystem/ExplosionEffectManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace ExplosionSystem
+{
+	public class ExplosionEffectManager : MonoBehaviour
+	{
+		[field: SerializeField]
+		private GameObject ExplosionPrefab { get; set; }
+		[field: SerializeField]
+		private AnimationClip ExplosionAnimationClip { get; set; }
+		
+		private static readonly int EXPLODE = Animator.StringToHash("EXPLODE");
+
+		public async UniTaskVoid MakeExplosion (Vector3 position)
+		{
+			GameObject newExplosion = Instantiate(ExplosionPrefab, position, Quaternion.identity);
+			newExplosion.GetComponent<Animator>().SetTrigger(EXPLODE);
+
+			await UniTask.Delay(TimeSpan.FromSeconds(ExplosionAnimationClip.length));
+			
+			Destroy(newExplosion);
+		}
+	}
+}
