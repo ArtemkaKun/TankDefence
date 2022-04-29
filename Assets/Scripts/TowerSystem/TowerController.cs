e528778b494691f9df8a7e876202ec457d6ef479
Artem Yurchenko <zumikq78@gmail.com>
2022-04-29 21:29:27 +0200

    TowerController class - animation controlling logic was set up

--- Assets/Scripts/TowerSystem/TowerController.cs
+++ Assets/Scripts/TowerSystem/TowerController.cs
@@ -6,6 +6,11 @@ namespace TowerSystem
 {
 	public class TowerController : MonoBehaviour
 	{
+		[field: SerializeField]
+		private Animator TowerAnimator { get; set; }
+		
+		private static readonly int IS_FIRING = Animator.StringToHash("IsFiring");
+
 		private void OnEnable()
 		{
 			TouchSimulation.Enable();
@@ -13,12 +18,16 @@ namespace TowerSystem
 
 		private void Update ()
 		{
-			if (Touchscreen.current.primaryTouch.isInProgress == true)
+			bool isScreenTouched = Touchscreen.current.primaryTouch.isInProgress;
+			
+			if (isScreenTouched == true)
 			{
 				Vector3 calculatedLookAtVector = Camera.main.ScreenToWorldPoint(Touchscreen.current.primaryTouch.position.ReadValue()) - transform.position;
 				calculatedLookAtVector.z = 0;
 				transform.up = calculatedLookAtVector;
 			}
+			
+			TowerAnimator.SetBool(IS_FIRING, isScreenTouched);
 		}
 	}
 }
