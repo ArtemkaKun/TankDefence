ee3f460e74816125c929ba02b17189db37e1414e
Artem Yurchenko <zumikq78@gmail.com>
2022-04-29 21:09:58 +0200

    TowerController class was implemented

---
+++ Assets/Scripts/TowerSystem/TowerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+namespace TowerSystem
+{
+	public class TowerController : MonoBehaviour
+	{
+		private void OnEnable()
+		{
+			TouchSimulation.Enable();
+		}
+
+		private void Update ()
+		{
+			if (Touchscreen.current.primaryTouch.isInProgress == true)
+			{
+				Vector3 calculatedLookAtVector = Camera.main.ScreenToWorldPoint(Touchscreen.current.primaryTouch.position.ReadValue()) - transform.position;
+				calculatedLookAtVector.z = 0;
+				transform.up = calculatedLookAtVector;
+			}
+		}
+	}
+}
