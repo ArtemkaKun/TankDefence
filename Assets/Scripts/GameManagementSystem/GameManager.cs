31dbab961656f74a39fed4c5069df57a83b6e827
Artem Yurchenko <zumikq78@gmail.com>
2022-04-30 22:05:12 +0200

    GameManager class - control GameOver screen logic was implemented

--- Assets/Scripts/GameManagementSystem/GameManager.cs
+++ Assets/Scripts/GameManagementSystem/GameManager.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace PlayerSystem
 {
 	public class GameManager : MonoBehaviour
 	{
 		[field: SerializeField]
+		private UIDocument GameOverScreen { get; set; }
+		[field: SerializeField]
 		private uint MaxTanksToPass { get; set; }
 
 		private PlayerData PlayerData { get; } = new PlayerData();
 
+		private void Awake()
+		{
+			GameOverScreen.rootVisualElement.visible = false;
+		}
+		
 		public void ReactOnTankWasPassed ()
 		{
 			PlayerData.TanksPassed += 1;
+
+			if (PlayerData.TanksPassed == MaxTanksToPass)
+			{
+				GameOverScreen.rootVisualElement.visible = true;
+			}
 		}
 		
 		public void ReactOnTankWasDestroyed ()
