2c9ac9490d7f21d041486d92320afa9f64007fb4
Artem Yurchenko <zumikq78@gmail.com>
2022-04-30 21:37:55 +0200

    GameManager and PlayerData classes were implemented

---
+++ Assets/Scripts/GameManagementSystem/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace PlayerSystem
+{
+	public class GameManager : MonoBehaviour
+	{
+		[field: SerializeField]
+		private uint MaxTanksToPass { get; set; }
+
+		private PlayerData PlayerData { get; } = new PlayerData();
+
+		public void ReactOnTankWasPassed ()
+		{
+			PlayerData.TanksPassed += 1;
+		}
+		
+		public void ReactOnTankWasDestroyed ()
+		{
+			PlayerData.TanksDestroyed += 1;
+		}
+	}
+}
---
+++ Assets/Scripts/GameManagementSystem/PlayerData.cs
@@ -0,0 +1,8 @@
+﻿namespace PlayerSystem
+{
+	public class PlayerData
+	{
+		public uint TanksPassed { get; set; }
+		public uint TanksDestroyed { get; set; }
+	}
+}
